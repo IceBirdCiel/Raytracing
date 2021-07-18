5bac537920ddbc7035d1fa80d1a0c609cd963855
IceBirdCiel <cceline74@hotmail.fr>
2021-07-18 23:41:03 +0200

    Added Cube class

---
+++ src/Objects/Cube.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../Materials/Material.h"
+#include "../Math/Vector.h"
+#include "Object.h"
+
+class Cube : public Object
+{
+public:
+	Cube(Vector pos, Vector rot, float size, float width, float height, Material mat);
+	Cube();
+	Material getMaterial(const Point& p)const;
+	Ray getNormals(const Point& p, const Point& o)const;
+	bool intersect(const Ray& ray, Point& impact)const;
+	float min(const float a, const float b)const;
+	float max(const float a, const float b)const;
+
+private:
+	Material mat;
+	float width, height;
+};
+
